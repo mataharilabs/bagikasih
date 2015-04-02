1f94317c0d10a07e0bbe33babf1bbeb7dd10b5b9
Ahadian Akbar <rabkanaidaha@gmail.com>
2015-04-02 14:13:05 +0700

    Add plugin

--- app/views/bagikasih/edit_profile/index.blade.php
+++ app/views/bagikasih/edit_profile/index.blade.php
@@ -9,6 +9,8 @@
 @stop
 @section('sidebar')
 
+{{ HTML::style('jasny-bootstrap/css/jasny-bootstrap.min.css') }}
+{{ HTML::script('jasny-bootstrap/js/jasny-bootstrap.min.js') }}
     <div class="container">
 
         <div class="row">
@@ -27,12 +29,20 @@
           <div class="panel-body col-lg-3">
               <div class="panel panel-default">
                 <div class="panel-body">
-                  <p><img src="/assets/assets/img/ava.png" class="img-polaroid img-rounded" style="max-width:100%;height:auto;"></p>
-                  <p><a href="#">Change Profile Photo</a></p>
+                  <div class="fileinput fileinput-new" data-provides="fileinput">
+                    <div class="fileinput-new thumbnail" style="max-width:100%;height:auto;">
+                      <img data-src="" src="/assets/assets/img/ava.png">
+                    </div>
+                    <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
+                    <div>
+                      <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span><input type="file" ></span>
+                      <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                    </div>
+                  </div>
                 </div>
               </div>
           </div>  
-
+          
           <div class="panel-body col-lg-9">
               <div class="panel panel-default">
                 <div class="panel-body">
