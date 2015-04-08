c20d18ebac33708f294c6cfa3db3b8cb6d858bd8
samzjf <samz_jf@yahoo.com>
2015-04-08 15:19:38 +0700

    photo slider

--- app/views/bagikasih/social-target/detail.blade.php
+++ app/views/bagikasih/social-target/detail.blade.php
@@ -53,41 +53,9 @@
 
 	<div class="row">
 		
-		<div class="col-lg-4 col-md-4 col-sm-6 hidden-xs">
-			<div class="panel panel-default">
-				<div class="panel-body">
-
-					@if (count($photos) > 0)
-						<a class="arrow-left" href="#"></a> 
-						<a class="arrow-right" href="#"></a>
-						<div class="swiper-container">
-							<div class="swiper-wrapper">
-
-								{{-- prioritize the default photo --}}
-								@if ($social_target->default_photo_id)
-								<div class="swiper-slide">
-									<img style="width:100%; height:100%;" class="img-polaroid img-rounded" src="{{ url('photos') }}/{{ $social_target->default_photo_id }}.jpg">
-								</div>
-								@endif
-							
-								@foreach ($photos as $photo)
-									@if ($photo->id != $social_target->default_photo_id)
-									<div class="swiper-slide">
-										<img style="width:100%; height:100%;" class="img-polaroid img-rounded" src="{{ url('photos') }}/{{ $photo->id }}.jpg">
-									</div>
-									@endif
-								@endforeach
-							
-							</div>
-						</div>
-						<div class="pagination"></div>
-					@else
-						<img style="width:100%; height:100%;" class="img-polaroid img-rounded" src="{{ url('photos') }}/default.jpg">
-					@endif
-
-				</div>
-			</div>
-		</div>
+		<!-- Photo Slider -->
+		@include('bagikasih.photo-slider')
+		<!-- Photo Slider - Selesai -->
 
 		<div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
 			<div class="panel panel-default">
