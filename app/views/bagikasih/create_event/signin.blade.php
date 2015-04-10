2ccbe231192cd773c88279f24241e9b4add258f1
Ahadian Akbar <rabkanaidaha@gmail.com>
2015-04-10 14:24:40 +0700

    Restructur Daftar Event

---
+++ app/views/bagikasih/create_event/signin.blade.php
@@ -0,0 +1,38 @@
+{{ Form::open(array('route' => 'update_profile','method'=>'POST', 'files'=>true,'class'=>"form-horizontal")) }}
+<div class="modal-body">
+  <fieldset>
+    <hr>Log in dengan akun terdaftar<p>
+    <div class="form-group">
+      <div class="col-lg-12">
+        <div class="input-group margin-bottom-sm">
+          <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+          <input class="form-control" type="text" id="email" name="email" placeholder="Email address">
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-lg-12">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+          <input class="form-control" type="password" id="password" name="password" placeholder="Password">
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-lg-6">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Ingat saya
+          </label>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <button type="submit" class="btn btn-primary" style="width:100%;"><i class="fa fa-sign-in"></i> Log In</button>
+      </div>
+    </div>
+  </fieldset>
+</div>
+<div class="modal-footer">
+  Tidak punya akun? <a href="#modal-signup" data-toggle="modal" data-dismiss="modal">Daftar</a>
+</div>
+</form>
