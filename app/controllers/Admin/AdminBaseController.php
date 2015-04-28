6f1c3e06ab1ec50211680939b3cc3aaa8fc613df
Ahadian Akbar <rabkanaidaha@gmail.com>
2015-04-28 13:30:49 +0700

    Signin filter signout

--- app/controllers/Admin/AdminBaseController.php
+++ app/controllers/Admin/AdminBaseController.php
@@ -15,4 +15,52 @@ class AdminBaseController extends BaseController {
 		}
 	}
 
+	public function admin(){
+
+		if (Request::isMethod('post')){
+			$rules = array(
+	        	'email'            => 'required|email',     // required and must be unique in the ducks table
+	    	    'password'         => 'required',
+		    );
+
+		    $validator = Validator::make(Input::all(), $rules);
+
+		    if ($validator->fails()) {
+		    	$messages = $validator->messages();
+	        	return Redirect::route('signin')->withErrors($validator);
+		    }else{
+
+		    	$input = array(
+	        		'email'            => Input::get('email'),     // required and must be unique in the ducks table
+	    	    	'password'         => md5(Input::get('password')),
+		    	); 
+
+		    	$login = Auth::attempt($input);
+		    	if($login){
+		    		// return Auth::user()->role;
+		    		if(Auth::user()->role == 1){
+		    			return Redirect::route('admin.dashboard');
+		    		}
+		    		else{
+						Auth::logout();
+						Session::flush();
+		    			Session::flash('gagal','Username yang anda masukan bukan administrator');
+		    			return Redirect::route('signin');
+		    		}
+		    	}else{
+		    		Session::flash('gagal','Username atau email salah');
+		    		return Redirect::route('signin');
+		    	}
+
+		    }
+		}
+		return View::make('admin.includes.signin');
+	}
+
+	public function signout(){
+		Auth::logout();
+		Session::flush();
+		return Redirect::route('signin');
+	}
+
 }
