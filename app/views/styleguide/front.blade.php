265ab628df643b050d9eae64af67970e61037abc
bedeute <bedeute@gmail.com>
2015-05-21 20:44:27 +0700

    update landing page

--- app/views/styleguide/front.blade.php
+++ app/views/styleguide/front.blade.php
@@ -29,35 +29,173 @@
 <div class="container">
   <h1 class="text-center">Situs dimana kamu bisa melakukan aksi nyata terhadap kehidupan sosial di dalam maupun di luar lingkungan anda dengan cara yang mudah dan mengasyikkan</h1>
 
-  <div class="showbar clearfix">
-    <div class="card">
-      <div class="card-head">
-        <div class="card-cover" style="background: transparent url(http://placeimg.com/640/480/any);">
-          <div class="card-author">
-            <img src="http://placeimg.com/46/46/any">
-            <h3>Anderson Silva</h3>
+  <div class="showbar">
+    <div class="showbar-inner clearfix">
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-head">
+            <a href="#" class="card-cover" style="background: transparent url(http://placeimg.com/222/222/any);">
+            </a>
+            <a href="#" class="card-author">
+              <img src="http://placeimg.com/46/46/any" class="author-pic">
+              <span class="author-name">Anderson Silva</span>
+            </a>
           </div>
-        </div>
-      </div>
-      <div class="card-body">
-        <h2>Customer metrics prototype learning curve</h2>
-        <div class="card-stats">
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%;">
-              <span class="sr-only">80% Complete</span>
+          <div class="card-body">
+            <a class="card-title" href="#">Customer metrics prototype learning curve</a>
+            <div class="card-stats clearfix">
+              <div class="progress progress-striped">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%;">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+              <div class="card-stats-text">
+                <p class="card-stats-hilite">Terkumpul <strong>IDR 25.000.<span>000</span></strong></p>
+                <p class="card-stats-lolite">Dari total dana <strong>IDR 25.000.<span>000</span></strong></p>
+              </div>
+              <div class="card-stats-symbol text-center">
+                <i class="fa fa-users"></i>
+                <span>20</span>
+              </div>
             </div>
+            <p>
+              Gen-z responsive web design paradigm shift equity burn rate pitch sales infrastructure ecosystem.
+              Disruptive holy grail value proposition long tail handshake lean startup buzz graphical user interface
+              strategy release backing research & development founders
+            </p>
+          </div>
+          <div class="card-footer">
+            <i class="fa fa-map-marker"></i>&nbsp;Surabaya, Jawa Timur
           </div>
         </div>
-        <p>
-          Gen-z responsive web design paradigm shift equity burn rate pitch sales infrastructure ecosystem.
-          Disruptive holy grail value proposition long tail handshake lean startup buzz graphical user interface
-          strategy release backing research & development founders
-        </p>
       </div>
-      <div class="card-footer">
-        <i class="fa fa-map-marker"></i>&nbsp;Surabaya, Jawa Timur
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-head">
+            <a href="#" class="card-cover" style="background: transparent url(http://placeimg.com/222/222/any);">
+            </a>
+            <a href="#" class="card-author">
+              <img src="http://placeimg.com/46/46/any" class="author-pic">
+              <span class="author-name">Anderson Silva</span>
+            </a>
+          </div>
+          <div class="card-body">
+            <a class="card-title" href="#">Customer metrics prototype learning curve</a>
+            <div class="card-stats clearfix">
+              <div class="progress progress-striped">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%;">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+              <div class="card-stats-text">
+                <p class="card-stats-hilite">Terkumpul <strong>IDR 25.000.<span>000</span></strong></p>
+                <p class="card-stats-lolite">Dari total dana <strong>IDR 25.000.<span>000</span></strong></p>
+              </div>
+              <div class="card-stats-symbol text-center">
+                <i class="fa fa-users"></i>
+                <span>20</span>
+              </div>
+            </div>
+            <p>
+              Gen-z responsive web design paradigm shift equity burn rate pitch sales infrastructure ecosystem.
+              Disruptive holy grail value proposition long tail handshake lean startup buzz graphical user interface
+              strategy release backing research & development founders
+            </p>
+          </div>
+          <div class="card-footer">
+            <i class="fa fa-map-marker"></i>&nbsp;Surabaya, Jawa Timur
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-head">
+            <a href="#" class="card-cover" style="background: transparent url(http://placeimg.com/222/222/any);">
+            </a>
+            <a href="#" class="card-author">
+              <img src="http://placeimg.com/46/46/any" class="author-pic">
+              <span class="author-name">Anderson Silva</span>
+            </a>
+          </div>
+          <div class="card-body">
+            <a class="card-title" href="#">Customer metrics prototype learning curve</a>
+            <div class="card-stats clearfix">
+              <div class="progress progress-striped">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%;">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+              <div class="card-stats-text">
+                <p class="card-stats-hilite">Terkumpul <strong>IDR 25.000.<span>000</span></strong></p>
+                <p class="card-stats-lolite">Dari total dana <strong>IDR 25.000.<span>000</span></strong></p>
+              </div>
+              <div class="card-stats-symbol text-center">
+                <i class="fa fa-users"></i>
+                <span>20</span>
+              </div>
+            </div>
+            <p>
+              Gen-z responsive web design paradigm shift equity burn rate pitch sales infrastructure ecosystem.
+              Disruptive holy grail value proposition long tail handshake lean startup buzz graphical user interface
+              strategy release backing research & development founders
+            </p>
+          </div>
+          <div class="card-footer">
+            <i class="fa fa-map-marker"></i>&nbsp;Surabaya, Jawa Timur
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-head">
+            <a href="#" class="card-cover" style="background: transparent url(http://placeimg.com/222/222/any);">
+            </a>
+            <a href="#" class="card-author">
+              <img src="http://placeimg.com/46/46/any" class="author-pic">
+              <span class="author-name">Anderson Silva</span>
+            </a>
+          </div>
+          <div class="card-body">
+            <a class="card-title" href="#">Customer metrics prototype learning curve</a>
+            <div class="card-stats clearfix">
+              <div class="progress progress-striped">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%;">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+              <div class="card-stats-text">
+                <p class="card-stats-hilite">Terkumpul <strong>IDR 25.000.<span>000</span></strong></p>
+                <p class="card-stats-lolite">Dari total dana <strong>IDR 25.000.<span>000</span></strong></p>
+              </div>
+              <div class="card-stats-symbol text-center">
+                <i class="fa fa-users"></i>
+                <span>20</span>
+              </div>
+            </div>
+            <p>
+              Gen-z responsive web design paradigm shift equity burn rate pitch sales infrastructure ecosystem.
+              Disruptive holy grail value proposition long tail handshake lean startup buzz graphical user interface
+              strategy release backing research & development founders
+            </p>
+          </div>
+          <div class="card-footer">
+            <i class="fa fa-map-marker"></i>&nbsp;Surabaya, Jawa Timur
+          </div>
+        </div>
       </div>
     </div>
+    <div class="showbar-pagination clearfix">
+      <div class="showbar-pagination-buttons pull-right">
+        <a href="#"><i class="fa fa-chevron-left"></i></a>
+        <a href="#"><i class="fa fa-chevron-right"></i></a>
+      </div>
+    </div>
+  </div>
+
+  <h1 class="text-center"><span style="font-size: 42px;">Mari Wujudkan Aksi Sosial</span><br>Menjadi lebih berarti dengan aksi sosial untuk berbagai kategori sosial di bawah ini.</h1>
+
+  <div class="category-hilite">
+    
   </div>
 
 </div>
