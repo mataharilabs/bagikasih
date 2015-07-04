4b51353b7ae7bff36c3323a3693ad169558bed48
samzjf <samz_jf@yahoo.com>
2015-07-04 13:38:03 +0700

    google analytics

--- app/views/bagikasih/theme/footer.blade.php
+++ app/views/bagikasih/theme/footer.blade.php
@@ -28,4 +28,15 @@ $currenturl = str_replace('/', '_', $currenturl);
 
 {{ HTML::script('assets/components/bootstrap/dist/js/bootstrap.min.js'); }}
 {{ HTML::script('js/credential.js'); }}
-{{ HTML::script('assets/assets/js/bagikasih.js'); }}
+{{ HTML::script('assets/assets/js/bagikasih.js'); 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64758398-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
