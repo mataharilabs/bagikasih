96d82429671a92ada974f2ad7d67965b89efcb8b
samzjf <samz_jf@yahoo.com>
2015-09-21 11:55:43 +0700

    fix bug

--- app/views/bagikasih/theme/navbar-logged-in.blade.php
+++ app/views/bagikasih/theme/navbar-logged-in.blade.php
@@ -2,7 +2,7 @@
       <div class="container">
 
         <div class="navbar-header">
-          <a href="http://www.bagikasih.com" class="navbar-brand"><img src="/assets/assets/img/logo.png"></a>          
+          <a href="{{ url() }}" class="navbar-brand"><img src="/assets/assets/img/logo.png"></a>          
           <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
--- app/views/bagikasih/theme/navbar.blade.php
+++ app/views/bagikasih/theme/navbar.blade.php
@@ -12,7 +12,7 @@
 <div class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
-      <a href="http://www.bagikasih.com" class="navbar-brand"><img src="/assets/assets/img/logo.png"></a>
+      <a href="{{ url() }}" class="navbar-brand"><img src="/assets/assets/img/logo.png"></a>
       <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
